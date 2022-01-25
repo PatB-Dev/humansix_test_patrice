5068ec1d7390eaa4c1e867080dbdf53dc1d4d38b
code-me-live <patricebelda@gmail.com>
2022-01-25 01:27:50 +0100

    Création twig de connexion + formulaire de connexion

---
+++ src/Form/LoginUserType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LoginUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username', TextType::class, ['data'=> 'Ton Login','required' => true])
+            ->add('password', TextType::class, ['data' => 'Mot de passe', 'required' => true])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
