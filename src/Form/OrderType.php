a1bd93717cdc691a8502b167f317c34393e134ad
code-me-live <patricebelda@gmail.com>
2022-01-29 03:05:48 +0100

    Rename

--- src/Form/OrderType.php
+++ src/Form/OrderType.php
@@ -7,7 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class Order1Type extends AbstractType
+class OrderType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
